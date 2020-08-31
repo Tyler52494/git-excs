b250db91f310e8a50b2a76c23fbcc714429f0461
Tyler Ammons <wammons23@gmail.com>
2020-08-31 12:59:19 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const userInput = document.querySelector("#user-input")
+const userInputButton = document.querySelector("#user-input-button")
+userInputButton.addEventListener("click" , function() {
+    const result = userInput.value.inlcudes('as')
+    console.log(result)
+
+})
+
+
+
+
+
+
+
+
+
+
+let mystring = "Hello World"
+let result
+
+result = mystring.includes('ll')
+console.log
+
+result = mystring.startsWith('He')
+console.log
+
+result = mystring.endsWith('rld')
+console.log
